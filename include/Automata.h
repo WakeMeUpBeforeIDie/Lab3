0635b15d54aac976c484b976503ef57636d8005c
AutumnAsh <aleksandr.kasyanov1337@mail.ru>
2026-04-06 17:45:39 +0300

    Update Automata.h

--- include/Automata.h
+++ include/Automata.h
@@ -4,21 +4,21 @@
 enum STATES {OFF, WAIT, ACCEPT, CHECK, COOK};
 class Automata {
 private:
-double cash;
-std::string[4] menu = ["coffee", "black tea", "green tea", "cola"];
-double[4] prices = [35.75, 20.34, 20.22, 134.47];
-STATES state;
+    double cash;
+    std::string menu[4];
+    double prices[4];
+    STATES state;
 public:
-Automata();
-void on();
-void off();
-void coin();
-void getMenu();
-STATES getState();
-void choice();
-bool check(int index);
-void cancel();
-void cook(int index);
-void finish();
+    Automata();
+    void on();
+    void off();
+    void coin();
+    void getMenu();
+    STATES getState();
+    void choice();
+    bool check(int index);
+    void cancel();
+    void cook(int index);
+    void finish();
 };
 #endif
