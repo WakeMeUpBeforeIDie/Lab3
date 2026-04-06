abe5ba6f35721af9aa7a17053582499ae3594a35
AutumnAsh <aleksandr.kasyanov1337@mail.ru>
2026-04-06 19:11:51 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,4 +1,72 @@
-// Copyright 2022 GHA Test Team
-
 #include <gtest/gtest.h>
 #include "Automata.h"
+TEST(AutomataTest, test1) {
+  Automata automata;
+  EXPECT_EQ(automata.getState(), OFF);
+}
+TEST(AutomataTest, test2) {
+  Automata automata;
+  automata.on();
+  EXPECT_EQ(automata.getState(), WAIT);
+}
+TEST(AutomataTest, test3) {
+  Automata automata;
+  automata.on();
+  automata.off();
+  EXPECT_EQ(automata.getState(), OFF);
+}
+TEST(AutomataTest, test4) {
+  Automata automata;
+  automata.on();
+  automata.coin();
+  EXPECT_EQ(automata.getState(), ACCEPT);
+}
+TEST(AutomataTest, test5) {
+  Automata automata;
+  automata.on();
+  automata.coin();
+  automata.choice();
+  EXPECT_EQ(automata.getState(), CHECK);
+}
+TEST(AutomataTest, test6) {
+  Automata automata;
+  automata.on();
+  for(int i = 0; i < 4; i++) {
+      automata.coin();
+  }
+  EXPECT_TRUE(automata.check(1));
+}
+TEST(AutomataTest, test7) {
+  Automata automata;
+  automata.on();
+  automata.coin();
+  EXPECT_FALSE(automata.check(0));
+}
+TEST(AutomataTest, test8) {
+  Automata automata;
+  automata.on();
+  automata.coin();
+  automata.choice();
+  automata.cancel();
+  EXPECT_EQ(automata.getState(), WAIT);
+}
+TEST(AutomataTest, test9) {
+  Automata automata;
+  automata.on();
+  for(int i = 0; i < 4; i++) {
+      automata.coin();
+  }
+  automata.choice();
+  EXPECT_TRUE(automata.check(1));
+  automata.cook(1);
+  EXPECT_EQ(automata.getState(), WAIT);
+}
+TEST(AutomataTest, test10) {
+  Automata automata;
+  automata.on();
+  automata.coin();
+  automata.coin();
+  automata.choice();
+  automata.cook(0);
+  EXPECT_EQ(automata.getState(), CHECK);
+}
