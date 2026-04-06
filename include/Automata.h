19e7a170d78dc3b713c73aa4af1fd021663c022b
AutumnAsh <aleksandr.kasyanov1337@mail.ru>
2026-04-06 15:41:54 +0300

    Create Automata.h

---
+++ include/Automata.h
@@ -0,0 +1,24 @@
+#ifndef AUTOMATA_H
+#define AUTOMATA_H
+#include <string>
+enum STATES = {OFF, WAIT, ACCEPT, CHECK, COOK};
+class Automata {
+private:
+double cash;
+std::string[] menu = ["coffee", "black tea", "green tea", "cola"];
+double[] prices = [35.75, 20.34, 20.22, 134.47];
+STATES state;
+public:
+Automata();
+void on();
+void off();
+void coin();
+void getMenu();
+void getState();
+void choice();
+bool check();
+void cancel();
+void cook();
+void finish();
+}
+#endif
